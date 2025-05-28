94e067d641b8a83f1b061d003391962a96dfdc5a
BaeJinYeol <108410316+BaeJinYeol@users.noreply.github.com>
2025-05-28 17:52:54 +0900

    Create 42586.cpp

---
+++ sort/42586.cpp
@@ -0,0 +1,29 @@
+// 스택, 기능개발, 레벨 2, cmath ceil올림
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+    vector<int> complete;
+
+    for (int i = 0; i < progresses.size(); i++) {
+        int x = ceil((100.0 - progresses[i]) / speeds[i]); // 소수점 올림
+        complete.push_back(x);
+    }
+
+    int day = complete[0];
+    answer.push_back(1);
+    for (int i = 1; i < complete.size(); i++) {
+        if (day < complete[i]) {
+            day = complete[i];
+            answer.push_back(1);
+        }
+        else {
+           answer.back()++;
+        }
+    }
+
+    return answer;
+}
