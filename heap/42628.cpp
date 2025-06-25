eeb6a2498ba298a88726ded6a7aef8d42234acd2
BaeJinYeol <108410316+BaeJinYeol@users.noreply.github.com>
2025-06-25 17:03:17 +0900

    Create 42628.cpp
    multiset

---
+++ heap/42628.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <set>
+using namespace std;
+
+vector<int> solution(vector<string> operations) {
+    multiset<int> ms;
+
+    for (auto op : operations) {
+        if (op[0] == 'I') {
+            int num = stoi(op.substr(2));
+            ms.insert(num);
+        } else if (!ms.empty()) {
+            if(op[2] == '1') {
+                auto it = prev(ms.end()); // 최대값
+                ms.erase(it);
+            } else {
+                auto it = ms.begin();   // 최소값
+                ms.erase(it);
+            }
+        }
+    }
+
+    if (ms.empty()) return {0,0};
+
+    return {*prev(ms.end()), *ms.begin()};
+}
